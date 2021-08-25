1916ef33bd5bf47f9d9234a6511b5f3fa521d57d
lucasdevelop <lucas.palomo@t-online.de>
2021-08-25 20:29:19 +0200

    add cli call with args

--- cli.js
+++ cli.js
@@ -1,15 +1,17 @@
 #! /usr/bin/env node
 
+let { access } = require("fs");
 let args = process.argv.slice(2);
 let readline = undefined;
 let rl = undefined;
-
 commandlist = [];
 
 addToCommadList("-help", () => {
   console.log("\nAvailable commands: ");
   commandlist.forEach((element) => {
-    console.log(element.name);
+    if (element.name !== "default") {
+      console.log(element.name);
+    }
   });
 });
 
@@ -21,14 +23,30 @@ addToCommadList("-output", (i) => {
   console.log("outputpath: " + args[i + 1]);
 });
 
+addToCommadList("default", (i) => {
+  access(args[i], function (error) {
+    if (error) {
+      console.log("Command Not found");
+    } else {
+      console.log("Directory exists.");
+    }
+  });
+});
+
 function addToCommadList(command, callback) {
   commandlist.push({ name: command, run: callback });
 }
 
 function runCommands(commandName, index) {
+  let hasRun = false;
   for (let i = 0; i < commandlist.length; i++) {
     if (commandlist[i].name === commandName) {
+      hasRun = true;
       commandlist[i].run(index);
+    }
+    if (i == commandlist.length - 1 && !hasRun) {
+      let c = commandlist.findIndex((x) => x.name === "default");
+      commandlist[c].run(index);
     }
   }
 }
@@ -47,21 +65,28 @@ function isInArgs(name, myargs) {
   });
   return result;
 }
-
 console.log(args);
 
 if (args.length <= 0) {
+  cli();
+} else {
+  args.forEach((element, i) => {
+    runCommands(element, i);
+  });
+}
+
+function cli() {
   readline = require("readline");
   rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false,
   });
-}
 
-rl.on("line", function (line) {
-  args = stringToArgs(line);
-  args.forEach((element, i) => {
-    runCommands(element, i);
+  rl.on("line", function (line) {
+    args = stringToArgs(line);
+    args.forEach((element, i) => {
+      runCommands(element, i);
+    });
   });
-});
+}
